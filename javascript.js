6480680b0f5c5f7f4435fd533ae84a5e81d2a689
Jayanta66 <jayhalder18@gmail.com>
2024-03-17 13:45:07 +0530

    guess my number

---
+++ javascript.js
@@ -0,0 +1,65 @@
+'use strict';
+
+
+let secretNumber = Math.trunc(Math.random() * 1) + 3;
+let score = 3;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number! You win $500');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+
+
+     /* displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!'); */
+      displayMessage(guess > secretNumber ? '📈 !' : '📉 !');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#f7051d';
+      document.querySelector('.number').style.width = '30rem';
+    }
+  }
+
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#0576f7';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+
+            function Redirect() {
+               window.location = "index.html";
+            }
---
+++ jss.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'COrrect Number';
+
+document.querySelector('.number').textContent = 5;
+
+document.querySelector('.score').textContent = 5;
